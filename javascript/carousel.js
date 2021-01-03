8e4f367e488c231f6a63e2ad96896c5f504db8d6
Do Minh Khanh <dominhkhanh99@gmail.com>
2021-01-03 21:18:08 +0700

    first commit

---
+++ javascript/carousel.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+
+    nav: false,
+
+    responsive: {
+        0: {
+            items: 6
+        },
+
+        1000: {
+            items: 6
+        }
+    }
+})
